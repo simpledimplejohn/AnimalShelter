8d2e94884a1104d4aad79526ca956c5148dd43b4
Simple Dimple John <simpledimplejohn@gmail.com>
2021-08-20 14:38:26 -0700

    update AnimalShelterContext to add 20 bunnies

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -12,11 +12,26 @@ namespace AnimalShelter.Models
         {
             builder.Entity<Animal>()
                 .HasData(
-                    new Animal { AnimalId = 1, Name = "Fifi", Species = "Mutt", Age = 3, Gender = "Female" },
+                    new Animal { AnimalId = 1, Name = "Fifi", Species = "Mutt", Age = 1, Gender = "Female" },
                     new Animal { AnimalId = 2, Name = "Bobo", Species = "Lizard", Age = 1, Gender = "Unknown" },
                     new Animal { AnimalId = 3, Name = "Smol", Species = "Bunny", Age = 1, Gender = "Female" },
                     new Animal { AnimalId = 4, Name = "vSmol", Species = "Bunny", Age = 1, Gender = "Male" },
-                    new Animal { AnimalId = 5, Name = "Gus", Species = "Bunny", Age = 22, Gender = "Male" }
+                    new Animal { AnimalId = 5, Name = "Gus", Species = "Bunny", Age = 2, Gender = "Male" }
+                    new Animal { AnimalId = 6, Name = "Commando", Species = "Mutt", Age = 3, Gender = "Female" },
+                    new Animal { AnimalId = 7, Name = "Bubba", Species = "Lizard", Age = 1, Gender = "Unknown" },
+                    new Animal { AnimalId = 8, Name = "The Onion", Species = "Bunny", Age = 1, Gender = "Female" },
+                    new Animal { AnimalId = 9, Name = "Big Nose", Species = "Bunny", Age = 1, Gender = "Male" },
+                    new Animal { AnimalId = 10, Name = "Hungry", Species = "Bunny", Age = 2, Gender = "Male" }
+                    new Animal { AnimalId = 11, Name = "Muffin", Species = "Mutt", Age = 3, Gender = "Female" },
+                    new Animal { AnimalId = 12, Name = "Dumbo", Species = "Lizard", Age = 1, Gender = "Unknown" },
+                    new Animal { AnimalId = 13, Name = "Potato", Species = "Bunny", Age = 1, Gender = "Female" },
+                    new Animal { AnimalId = 14, Name = "Matchstick", Species = "Bunny", Age = 1, Gender = "Unknown" },
+                    new Animal { AnimalId = 15, Name = "Orion", Species = "Bunny", Age = 1, Gender = "Male" }
+                    new Animal { AnimalId = 16, Name = "Peggy", Species = "Mutt", Age = 3, Gender = "Female" },
+                    new Animal { AnimalId = 17, Name = "Herb", Species = "Lizard", Age = 1, Gender = "Unknown" },
+                    new Animal { AnimalId = 18, Name = "Kevin", Species = "Bunny", Age = 1, Gender = "Male" },
+                    new Animal { AnimalId = 19, Name = "Daniel", Species = "Bunny", Age = 1, Gender = "Male" },
+                    new Animal { AnimalId = 20, Name = "Steve", Species = "Bunny", Age = 2, Gender = "Male" }
                 );
         }
         public DbSet<Animal> Animals { get; set; }
